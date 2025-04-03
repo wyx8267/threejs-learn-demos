fbe3c591b102d020209c62debf95099f31927946
wangyuxin <wangyuxin@iscmtech.com>
2025-04-03 15:16:22 +0800

    随机山脉地形

--- mountain-terrain/index.js
+++ mountain-terrain/index.js
@@ -2,26 +2,28 @@ import * as THREE from 'three';
 import {
     OrbitControls
 } from 'three/addons/controls/OrbitControls.js';
-import mesh from './mesh.js';
+import mesh, { updatePosition } from './mesh.js';
 
 const scene = new THREE.Scene();
 
 scene.add(mesh);
 
 const axesHelper = new THREE.AxesHelper(200);
-scene.add(axesHelper);
+// scene.add(axesHelper);
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 const camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
-camera.position.set(200, 200, 200);
+camera.position.set(600, 300, 200);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height)
 
 function render() {
+    updatePosition();
+    mesh.rotateZ(0.0015);
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
--- mountain-terrain/mesh.js
+++ mountain-terrain/mesh.js
@@ -1,6 +1,21 @@
 import * as THREE from 'three';
+import { createNoise2D } from 'simplex-noise';
 
-const geometry = new THREE.PlaneGeometry(300, 300, 10, 10);
+const geometry = new THREE.PlaneGeometry(3000, 3000, 100, 100);
+
+const noise2D = createNoise2D();
+
+export function updatePosition() {
+    const positions = geometry.attributes.position;
+    for (let i = 0; i < positions.count; i++) {
+        const x = positions.getX(i);
+        const y = positions.getY(i);
+        const z = noise2D(x / 300, y / 300) * 50;
+        const sinNum = Math.sin(Date.now() * 0.003 + x * 0.05) * 15;
+        positions.setZ(i, z + sinNum);
+    }
+    positions.needsUpdate = true;
+}
 
 const material = new THREE.MeshBasicMaterial({
     color: new THREE.Color('orange'),
