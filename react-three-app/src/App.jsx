e986de08992ef54e29fcc07455737551a762fa9a
wangyuxin <wangyuxin@iscmtech.com>
2025-06-23 13:52:51 +0800

    react项目集成three.js

---
+++ react-three-app/src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { init } from "./3d-init";
+
+function App() {
+  const jumpRef = useRef(() => {});
+  const [str, setStr] = useState("");
+
+  useEffect(() => {
+    const dom = document.getElementById("content");
+    const { jump } = init(dom, setStr);
+    jumpRef.current = jump;
+    return () => {
+      dom.innerHTML = "";
+    };
+  }, []);
+
+  return (
+    <div>
+      <div id="header">React 和 Three.js</div>
+      <div id="main">
+        <div id="content"></div>
+        <div id="operate">
+          <button
+            onClick={() => {
+              jumpRef.current("red");
+            }}
+          >
+            红色
+          </button>
+          <button
+            onClick={() => {
+              jumpRef.current("green");
+            }}
+          >
+            绿色
+          </button>
+          <button
+            onClick={() => {
+              jumpRef.current("blue");
+            }}
+          >
+            蓝色
+          </button>
+          <div>{str}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
